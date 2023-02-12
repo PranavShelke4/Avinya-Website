d2790c4562952ef64ef0d30091e03d929b84b355
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-02-12 17:23:44 +0530

    change

--- src/component/HomePage/perfectlySection.js
+++ src/component/HomePage/perfectlySection.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './perfectlySection.css'
 import perfectly from '../../assets/perfectly.png'
-import vIcon from '../../assets/videoIcon.svg'
 function PerfectlySection() {
     return (
       <div className='PerSection'>
         <h1>how to use the app perfectly</h1>
         <div className='perbody'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus.</div>
-        <img src={perfectly} width={700}/>
+        <img src={perfectly} alt='Tada' width={700}/>
         {/* <img src={vIcon} className='vIcon' width={50}/> */}
         </div>
         )
