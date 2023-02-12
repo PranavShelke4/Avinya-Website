a30d46da271d8859f91d56e67cebe17fead0a469
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-02-12 17:56:09 +0530

    .

--- src/component/HomePage/perfectlySection.js
+++ src/component/HomePage/perfectlySection.js
@@ -6,7 +6,7 @@ function PerfectlySection() {
       <div className='PerSection'>
         <h1>how to use the app perfectly</h1>
         <div className='perbody'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus.</div>
-        <img src={perfectly} alt='Tada' width={700}/>
+        <img src={perfectly} alt='Tada' width={700}/> 
         {/* <img src={vIcon} className='vIcon' width={50}/> */}
         </div>
         )
