9f0c3d8b7ccd1ff4cc1620cf0abff08b6ec884d6
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-02-12 18:21:25 +0530

    perfect

--- src/component/HomePage/perfectlySection.js
+++ src/component/HomePage/perfectlySection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import './perfectlySection.css'
+import './perfectlySection.css'
 import perfectly from '../../assets/perfectly.png'
 function PerfectlySection() {
     return (
@@ -7,7 +7,6 @@ function PerfectlySection() {
         <h1>how to use the app perfectly</h1>
         <div className='perbody'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus.</div>
         <img src={perfectly} alt='Tada' width={700}/> 
-        {/* <img src={vIcon} className='vIcon' width={50}/> */}
         </div>
         )
 }
