2b99e7f268a8f420549e41186a99d5010e6f9c54
SwarupVishwas18 <swarupvishwas000@gmail.com>
2023-02-12 19:32:59 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import AboutSection from "./component/HomePage/AboutSection";
 import FeaturesSection from './component/HomePage/FeaturesSection';
 import PerfectlySection from './component/HomePage/perfectlySection';
 import DemoSection from "./component/HomePage/DemoSection";
-<<<<<<< HEAD
 import Footer from "./component/footer";
-=======
 import DownloadSection from "./component/HomePage/DownloadSection";
->>>>>>> 54822a9b178e1e3090895f8e0c5bb490d6f4090a
 
 function App() {
   return (
