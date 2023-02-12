0e1b3d43660b029ad8ca11bf685fdc74d337c39a
Pranav shelke <88943865+PranavShelke4@users.noreply.github.com>
2023-02-12 12:17:21 +0000

    demo section done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Navbar from './component/navbar';
-import HeroSection from './component/HomePage/HeroSection';
-import AboutSection from './component/HomePage/AboutSection';
+import React from "react";
+import Navbar from "./component/navbar";
+import HeroSection from "./component/HomePage/HeroSection";
+import AboutSection from "./component/HomePage/AboutSection";
+import DemoSection from "./component/HomePage/DemoSection";
 
 function App() {
-
   return (
-
-      <diV>
-        <Navbar />
-        <HeroSection/>
-        <AboutSection/>
-      </diV>
-
+    <diV>
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <DemoSection />
+    </diV>
   );
 }
 
---
+++ src/component/HomePage/FeaturesSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./FeaturesSection.css";
+
+function FeaturesSection() {
+  return (
+    <div>FeaturesSection</div>
+  )
+}
+
+export default FeaturesSection
